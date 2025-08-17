0bd71c9b57c4080586b62d0fc1e80f7ef950a030
Celestial2B <theyorhaend@gmail.com>
2025-08-17 09:16:38 -0600

    Documentacion de Manager

--- practica1/src/main/java/Main.java
+++ practica1/src/main/java/Main.java
@@ -1,27 +1,17 @@
 package main.java;
-
 import main.java.airline.*;
-import java.util.List;
 
 public class Main{
-
     public static void main(String[] args){
-
         Aerolinea aerolinea = Manager.generaAerolinea();
-        
         System.out.println("Vuelos de la aerolinea:");
         for(Vuelo vuelo: aerolinea.getVuelos()){
             System.out.println("* " + vuelo.toString());
         }
-
-
         System.out.println("\nVuelos con origen Ciudad de México:");
-        
         for(Vuelo vuelo: aerolinea.buscaOrigen("Ciudad de México")){
             System.out.println("▣ " + vuelo.toString());
         }
-
-
         System.out.println("\nDuración promedio de todos los vuelos: " + aerolinea.duracionPromedio() + "min");
     }
 }
--- practica1/src/main/java/airline/Manager.java
+++ practica1/src/main/java/airline/Manager.java
@@ -1,18 +1,22 @@
 package main.java.airline;
-
 import main.java.common.ReaderWriter;
 
 public class Manager{
-
+    /**
+     * Genera una instacia de Aerolinea a partir de los datos
+     * contenidos en el archivos "vuelos.txt"
+     * 
+     * @return una instancia Aerolinea con los vuelos cargados desde el archivo.
+     * 
+     * @throws NullPointerException una línea del archivo es nula
+     * @throws ArrayIndexOutOfBoundsException una línea no tiene el número correcto de campos
+     * @throws NumberFormatException el campo de id o duracion no puede convertise en número
+     */
     public static Aerolinea generaAerolinea(){
-
         Aerolinea aerolinea = new Aerolinea();
-
         String[] lineas = ReaderWriter.read("vuelos.txt");
-
         for(String linea: lineas){
             String[] datosVuelo = linea.split(",");
-            
             try{
                 int id = Integer.parseInt(datosVuelo[0].trim());
                 String origen = datosVuelo[1].trim();
@@ -20,10 +24,8 @@ public class Manager{
                 String fecha = datosVuelo[3].trim();
                 String hora = datosVuelo[4].trim();
                 int duracion = Integer.parseInt(datosVuelo[5].trim());
-
                 Vuelo nuevoVuelo = new Vuelo(id, origen, destino, fecha, hora, duracion);
                 aerolinea.agregar(nuevoVuelo);
-
             } catch (NullPointerException e) {
                         System.err.println("Error Null: " + linea);
             } catch (ArrayIndexOutOfBoundsException e) {
@@ -31,11 +33,7 @@ public class Manager{
             } catch (NumberFormatException e) {
                         System.err.println("Error de formato: " + linea);
             }
-
         }
-
         return aerolinea;
     }
-
-
 }
